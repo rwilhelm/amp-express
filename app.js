76de38a7d0897dc152451c92836b837623decf7c
René Wilhelm <rene.wilhelm@gmail.com>
2012-12-11 05:43:26 +0100

    accidentaly removed route to /image

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/image', image.display);
+
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
 });

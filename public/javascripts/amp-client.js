9dd950bc9828373315328d12867d0e131b5f4cba
René Wilhelm <rene.wilhelm@gmail.com>
2012-12-13 06:17:03 +0100

    removed unneccesary stuff. refactored flickTrail. now using socket.io to get the arrays containing filenames.

--- public/javascripts/amp-client.js
+++ public/javascripts/amp-client.js
@@ -1,67 +1,64 @@
-/* TODO
- * function -> toggle class
- * function -> delay
- * function -> display picture trail (for i in a; do flick(i, time))
- */
-
 /*
- * Handle Keyboard Input (requires jQuery)
+ * amp-client.js
  *
+ * © 2012 René Wilhelm <rene.wilhelm@gmail.com>
+ * https://www.github.com/rwilhelm/amp-express
+ *
+ * vi: set ts=2 sw=2 sts=2
  */
 
-var  DOT    =  "/images/dot.jpg"
-var  NOISE  =  "/images/noise.jpg"
+var  characters  =  []
+,    pictures    =  []
 
-/* do something with a delay after */
-var delayAfter = function(f, s) {
-	return setTimeout(function() { f; }, s);
-}
+var socket = io.connect('http://localhost');
+socket.on('arrays', function (data) {
+	characters = data.characters;
+	pictures = data.pictures;
+});
 
-/* toggle hide-class on an image for a certain duration */
-var showImage = function(id, duration) {
-	return delayAfter($(id).toggleClass("hide"), delay);
-};
-
-var flick = function (div, timeout, callback) { // FIXME callbacks!
-	setTimeout(function() { $('#flick ' + div + ' img').toggleClass("hide"); }, timeout+500);
-	setTimeout(function() { $('#flick ' + div + ' img').toggleClass("hide"); }, timeout);
-	return;
-};
-
-var pickRandom = function (array) {
+var pickRandom = function(array) {
 	return array[Math.floor(Math.random()*array.length)].replace(/\public/, '');
 };
 
-var flickTrail = function () {
-	$('#picture img').attr('src', pickRandom(pictures));
-	$('#character img').attr('src', pickRandom(characters));
+/*
+ * noise -> dot -> picture -> character -> noise
+ */
 
-	flick("#dot", 500);
-	flick("#picture", 1000);
-	flick("#character", 1500);
-	flick("#noise", 2000);
-	return;
+var flickTrail = function() { // setTimeout(callback, delay, [arg], [...])
+	$('#flick img#noise').addClass("hide"); // noise off
+	$('#flick img#dot').removeClass("hide"); // dot on
+	setTimeout(function() {
+		$('#flick img#dot').addClass("hide"); // dot off
+		$('img#picture').attr('src', pickRandom(pictures)); // random picture
+		$('#flick img#picture').removeClass("hide"); // picture on
+		setTimeout(function() {
+			$('#flick img#picture ').addClass("hide"); // picture off
+			$('img#character').attr('src', pickRandom(characters)); // random character
+			$('#flick img#character ').removeClass("hide"); // character on
+			setTimeout(function() {
+				$('#flick img#character ').addClass("hide"); // character off
+				$('#flick img#noise ').removeClass("hide"); // noise on
+			},300); // character
+		},300); // picture
+	},300); // dot
 };
 
-$(function() {
-	$(window).keypress(function(e) {
-		switch(e.which) {
-			case 112: // lowercase p
-				//console.log("Yay, like! :) Keycode: " + e.which);
-				$('#right.button').toggleClass("active");
-				setTimeout(function() { $('#right.button').toggleClass("active"); }, 300);
-				flickTrail();
-				break;
-			case 113: // lowercase q
-				//console.log("Ugh, dislike. :/ Keycode: " + e.which);
-				$('#left.button').toggleClass("active");
-				setTimeout(function() { $('#left.button').toggleClass("active"); }, 300);
-				flickTrail();
-				break;
-			case 114: // lowercase r
-				flickTrail();
-				break;
-		}
-	});
+$(window).keypress(function(e) { // FIXME
+	switch(e.which) {
+		case 112: // lowercase p
+			//console.log("Yay, like! :) Keycode: " + e.which);
+			$('#right.button').toggleClass("active");
+			setTimeout(function() { $('#right.button').toggleClass("active"); }, 300);
+			flickTrail();
+			break;
+		case 113: // lowercase q
+			//console.log("Ugh, dislike. :/ Keycode: " + e.which);
+			$('#left.button').toggleClass("active");
+			setTimeout(function() { $('#left.button').toggleClass("active"); }, 300);
+			flickTrail();
+			break;
+		case 114: // lowercase r
+			flickTrail();
+			break;
+	}
 });
-
